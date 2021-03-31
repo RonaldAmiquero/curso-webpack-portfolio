78fa99405b02edc89ea92b14dd4d9280bf1a35c9
Ronald Amiquero <roamz.2017@gmail.com>
2021-03-30 19:31:21 -0500

    [ADD] Stats

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
-
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 module.exports = {
   // entry nos permite decir el punto de entrada de nuestra aplicación
   entry: "./src/index.js",
@@ -17,7 +18,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
-  watch: true,
+  devtool: "source-map",
   resolve: {
     // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
     extensions: [".js"],
@@ -79,5 +80,13 @@ module.exports = {
       ],
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true,
+  },
 };
